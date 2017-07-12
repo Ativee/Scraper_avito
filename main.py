6cab5c532c6ea31aeb24bd01b6a1f5e8d29b7b7f
Ativee <Amur@elitdv.ru>
2017-07-13 01:50:17 +0900

    первая залика проекта 0.35

--- main.py
+++ main.py
@@ -1,20 +1,85 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import requests
-# dfdfdfdzx zzxfffff
 
-url = 'https://www.avito.ru/profile'
-def Page_obj:
+"""
+Парсинг сайта 2ГИС для составления базы данных огранизаций 
 
-    page_obj = requests.get(urlopen())
-    if
-    page_kode = requests.get(urlopen())
-    if
-    return
+Алгоритм извлечения данных:
+
+1. Парсинг первой страницы сайта для составления
+    - рубрикатора
+    - списка городов
+    
+2. Структура извлекаемых данных:
+    - Город
+    - Наименование огранизации
+    - Номер телефона
+    - Адресс
+    - E-mail
+    - Сайт
+    - Смежные рубрики
+    - Режим работы
+
+    
+3. Запись экземпляра класса в базу данных.
+4. Потроение API для работы с базой данных.
+
+
+Алгоритм отслеживания изменений и тенденций:
+
+1. Проверка количества организаций по рубрикам
+2. Сводка открывшихся и закрывшихся организаций
+3. Сводка организаций проводящих рекламную компанию
+
+
+"""
+
+# Класс
+class Main_page:
+    url = 'https://www.2gis.ru/'
+    city = ''
+
+
+    def set(self, city):
+        self.city = str(city)
+        base_region = self.url + str(base)
+
+
+    def dfffd(self):
+        self.dfdf = dfdfd
+
+
+
+class Rec_DB
+
+
+
+class
+
+amur = Page_html()
+amur.set('blaga')
+
+print(amur.base_region)
 
 
 
 
-
-
-if __name__== '__main__':
+#
+#
+# def main():
+#     page_obj = requests.get(url)
+#     if page_obj.status_code != 200:
+#         print('Соединение не удалось, проверь правильность URL')
+#     else:
+#         print('Cоединение установлено \n', page_obj.status_code)
+#         return page_obj.text
+#
+# def soup():
+#
+#
+#
+#
+#
+# if __name__== '__main__':
+#     main()
