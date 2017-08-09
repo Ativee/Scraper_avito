ff5dde498b35581dcbd464bf7b9f61d3eb088021
Ativee <Amur@elitdv.ru>
2017-08-09 17:06:17 +0900

    gjt[fkb

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import lxml
 import sqlite3 as lite
 import os
 import requests
+import sys
 
 """
 Парсинг сайта 2ГИС для составления базы данных огранизаций 
@@ -38,84 +39,85 @@ import requests
 """
 #  список городов              https://2gis.ru/countries/global/blagoveshensk
 # спиок рубрик внутри города   https://2gis.ru/blagoveshensk/rubrics
+
 # Генерация URL для парсинга страницы
 class Url:
     rubric = ''
+
     def __init__(self, sity = 'blagoveshensk'):
         self.sity = sity
         self.entry_rubric = 'https://2gis.ru/' + sity + '/rubrics'
         self.entry_sity = 'https://2gis.ru/countries/global/' + sity
 
+
+    # Получение списка городов
     def get_sity_list(self):
         r = requests.get(self.entry_sity)
         html = r.text
         soup = BeautifulSoup(html, 'html.parser')
         # блок областного центра ******
         ul = soup.find_all('li', class_='world__listItem')
-        print(len(ul))
-        self.sity_link = []
-        self.sity_name = []
+
+        self.sities_links = []
+        self.sities_names = []
 
         for i in ul[0:-18]:
-            h2 = i.find('h2',class_='world__listItemName')
+            h2 = i.find('h2', class_='world__listItemName')
             a = h2.find('a')
             a_link = a.get('href')
             abs_link = 'https://2gis.ru' + a_link + '/rubrics'
-            self.sity_link.append(abs_link)
-            self.sity_name.append(h2.string)
+            self.sities_links.append(abs_link)
+            self.sities_names.append(h2.string)
             # print(h2.string)
             # print(abs_link)
 
-    def rec_sity_list(self):
+
+
+    # запись списка городов в базу данных
+    # def rec_sity_list(self):
         con = lite.connect('C:\\Users\\Елагин\\PycharmProjects\\Scraper_avito\\GIS.db')
-        with con:
-            cur = con.cursor()
-            cur.execute("INSERT INTO Sities_list (sity) VALUES('hhhh')")
-            # cur.execute("INSERT INTO Sities_list VALUES(link=:link, link )", {"sity": self.sity})
-            con.commit()
-
-        print('запись произведена')
-
-
-
+    #     with con:
+    #         cur = con.cursor()
+    #         try:
+    #
+    #         cur.execute("INSERT INTO Sities_list (sity) VALUES('hhhh')")
+    #         # cur.execute("INSERT INTO Sities_list VALUES(link=:link, link )", {"sity": self.sity})
+    #         con.commit()
+    #     print('запись произведена')
 
 
-
-
-
-
-        # for i in li:
-        #     h2 = i.find('h2', class_="world__listItemName")
-        #
-        #
-        #     print(h2)
-
-            # h2_a = h2.find('a')
-            # link = h2_a.get('href')
-            # li_sat = i.find_all('li', class_='world__settlementsItem')
-            # print('Областной центр: ', h2_text, 'Ссылка: ', link)
-            # for i in li_sat:
-            #     if li_sat is None:
-            #         print("Нет городов сателитов")
-            #     else:
-            #         i.find_all('a')
-            #         for z in i:
-            #             print(z)
-            #         print(len(z))
-
-            # print('',li_sat)
-
+# def rec(arg):
+#     con = lite.connect('C:\\Users\\Елагин\\PycharmProjects\\Scraper_avito\\GIS.db')
+#
+#     cur = con.cursor()
+#
+#     cur.execute("INSERT INTO Sities_list (sity) VALUES(?)", str(arg))
+#             # cur.execute("INSERT INTO Sities_list VALUES(link=:link, link )", {"sity": self.sity})
+#     con.commit()
+#     print('запись произведена')
+#     cur.close()
+#     con.close()
 
 
 
 def main():
+    print(os.getcwd())
+
     a = Url()
     print(a.entry_sity)
     a.get_sity_list()
+    print(len(a.sities_links))
+    print(a.sities_links)
+    print(len(a.sities_names))
+    print(a.sities_names)
+    for sity in a.sities_names:
+        print(sity)
+        rec(sity)
 
-    foi i in a.sity_link:
-    a.rec_sity_list()
-    print (a.sity_link[0:5])
+
+
+
+
 
 
 
