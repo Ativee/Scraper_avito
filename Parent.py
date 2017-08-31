ad814acec9f1f684995c1823c4ff3ecf1a8813fa
Ativee <Amur@elitdv.ru>
2017-08-31 17:01:44 +0900

    Подготовка к внесению элементов списков рубрик и городов.

--- Parent.py
+++ Parent.py
@@ -14,14 +14,26 @@ class Parent():
         self.parent_window = root
         self.parent_window.title('Программа парсинга 2Гис')
         self.parent_window.geometry('800x600+50+50')
+        self.btn = Button(root,text='кнопка', command= Child)
+        self.btn.pack()
+
 
         self.parent_window.mainloop()
 
 
 
+class Child(Toplevel):
+    def __init__(self):
+
+        self.title('Добавить доходы/расходы')
+        self.geometry('400x220+400+300')
+
+
 
 def Main():
-    root= Tk()
+    root = Tk()
+    root.title('Через функцию main')
+
     main = Parent(root)
 
 
